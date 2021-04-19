784807b648baf4d2bbfef8a4898555e98e1b3bc8
Choi-UngGyu <cdr982116@gmail.com>
2021-04-19 22:20:39 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App" />
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies = []
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading : false});
+    }, 6000);
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
