482ca45f80c226afe380273078913706467652f3
Choi-UngGyu <cdr982116@gmail.com>
2021-04-21 17:35:29 +0900

    #4.5 Cutting the summary

--- src/Movies.js
+++ src/Movies.js
@@ -11,7 +11,7 @@ function Movie({ year, title, summary, poster, genres }) {
             <ul className="genres">{genres.map((genre, index) => (
                 <li key={index} className="genres__genre">{genre}</li>
             ))}</ul>
-            <p className="movie__summary">{summary}</p>
+            <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
     </div>
 }
